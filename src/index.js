b175959f042bdb9ec0dfc0f3f46f14c40ffa031d
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-02 15:49:35 +0300

    feat: add style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import "./style.css";
+
 //Fetching Data from Weather API
 async function Weather() {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis ababa?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
   );
   const data = await response.json();
+  console.log(data);
   console.log(data.currentConditions.temp);
 }
 Weather();
