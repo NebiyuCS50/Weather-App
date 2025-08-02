8c972bc7e1b641b44f7194c75ecf2e15152a2b9d
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-02 15:15:30 +0300

    Add async Weather function to fetch and log  weather data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 async function Weather() {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis ababa?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
   );
   const data = await response.json();
-  console.log(data);
+  console.log(data.currentConditions.temp);
 }
+Weather();
