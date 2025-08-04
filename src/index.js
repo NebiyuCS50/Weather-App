998733550922c15fa9f0c28128c8beaba772c68c
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-04 13:48:28 +0300

    feat: add logic to api return data

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ export async function Weather(params) {
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(params)}?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
   );
   const data = await response.json();
-  console.log(data);
-  console.log(data.currentConditions.temp);
+  return data;
 }
 
 //Fecthing Data from Giphy API
 
 const container = document.querySelector(".container");
 const img = document.createElement("img");
-async function giphy() {
+export async function giphy(gifParam) {
+  const oldGif = container.querySelector(".giphy");
+  if (oldGif) container.removeChild(oldGif);
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=CHeuvC0jF2z4XQReQXW7b7zPtTmJIOeh&s=cats`,
+    `https://api.giphy.com/v1/gifs/translate?api_key=CHeuvC0jF2z4XQReQXW7b7zPtTmJIOeh&s=${gifParam || "weather"}`,
   );
   const data = await response.json();
   img.src = data.data.images.original.url;
@@ -25,7 +26,5 @@ async function giphy() {
   img.classList.add("giphy");
   container.appendChild(img);
 }
-giphy();
-
 //search bar functionality
 searchBar();
