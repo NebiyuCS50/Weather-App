e195c7dbeebc166b4dd00b01ab3d0e2821e41366
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-04 14:19:02 +0300

    feat: add logic to api return data

--- src/display.js
+++ src/display.js
@@ -5,11 +5,4 @@ const main = document.querySelector(".main");
 // Function to display weather data
 export function displayWeather(data) {
   if (!data) return;
-  main.textContent = `
-    <h1>${data.address}</h1>
-    <p>Temperature: ${data.currentConditions.temp}°F</p>
-    <p>Conditions: ${data.currentConditions.conditions}</p>
-    <p>Humidity: ${data.currentConditions.humidity}%</p>
-    <p>Wind Speed: ${data.currentConditions.windspeed} km/h</p>
-  `;
 }
