036d79dd09465abeb98690db117c52bc4465a370
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-04 13:47:58 +0300

    feat: add logic to image selection

--- src/search.js
+++ src/search.js
@@ -1,16 +1,29 @@
 import { Weather } from "./index.js";
+import { giphy } from "./index.js";
 //search bar
 function searchBar() {
   const searchBar = document.querySelector(".input");
-  searchBar.addEventListener("keyup", (e) => {
+  searchBar.addEventListener("keyup", async (e) => {
     if (e.key === "Enter") {
       const searchValue = searchBar.value;
-      console.log(searchValue);
+      let weatherData;
       if (searchValue) {
-        Weather(searchValue);
+        weatherData = await Weather(searchValue);
         searchBar.value = ""; // Clear the input after search
       } else {
-        Weather("Addis Ababa, Ethiopia");
+        weatherData = await Weather("Addis Ababa, Ethiopia");
+      }
+
+      if (weatherData.currentConditions.conditions == "Rain") {
+        giphy("rain");
+      } else if (weatherData.currentConditions.conditions == "Cloudy") {
+        giphy("cloudy");
+      } else if (weatherData.currentConditions.conditions == "Sunny") {
+        giphy("sunny");
+      } else if (weatherData.currentConditions.conditions == "Snow") {
+        giphy("snow");
+      } else {
+        giphy("weather");
       }
     }
   });
