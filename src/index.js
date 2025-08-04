e05a0a292d46ed468ef7fa6645430fc8f45b109a
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-04 11:45:24 +0300

    feat: add the search logic

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import "./style.css";
 
+//search bar
+const searchBar = document.querySelector(".input");
+searchBar.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    const searchValue = searchBar.value;
+    console.log(searchValue);
+    if (searchValue) {
+      Weather(searchValue);
+      searchBar.value = ""; // Clear the input after search
+    } else {
+      Weather("Addis Ababa, Ethiopia");
+    }
+  }
+});
+
 //Fetching Data from Weather API
-async function Weather() {
+async function Weather(params) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis ababa?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(params)}?key=6XVV6HUW6G6K9CK3RDG5H57B7`,
   );
   const data = await response.json();
   console.log(data);
   console.log(data.currentConditions.temp);
 }
-Weather();
 
 //Fecthing Data from Giphy API
 
+const container = document.querySelector(".container");
+const img = document.createElement("img");
 async function giphy() {
   const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=CHeuvC0jF2z4XQReQXW7b7zPtTmJIOeh&s=cats`,
   );
   const data = await response.json();
-  console.log(data);
+  img.src = data.data.images.original.url;
+  img.alt = "Giphy Image";
+  img.classList.add("giphy");
+  container.appendChild(img);
 }
 giphy();
